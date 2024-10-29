79454f4908fa4dda1f51b61104cc6afe5ab1c04c
Rashid Gizatullin <rashidg@halykbank.kz>
2024-10-29 11:55:22 +0500

    store data in the database

--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest-api/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -13,9 +17,23 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// todo: add it to a database later
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events (name, description, location, datetime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+
+	return err
 }
 
 func GetAllEvents() []Event {
