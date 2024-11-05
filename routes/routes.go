02512ddfad7cd1f0e92ecf277e1b2625356d765c
Rashid Gizatullin <rashidg@halykbank.kz>
2024-11-05 17:32:37 +0500

    add authenticate middleware

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,23 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/rest-api/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+	// You can add some middlewares by passing them as parameters
+	// server.POST("/events", middlewares.Authenticate, createEvent)
+
+	// Another way is to create a group with middlewares
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
